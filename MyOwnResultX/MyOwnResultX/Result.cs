4c7024e714841a867d836a3b1f0ae66a6842e366
Kanat Sadyrbekov <kanat890088@gmail.com>
2023-10-03 20:14:59 +0600

    Added static methods to Result

--- MyOwnResultX/MyOwnResultX/Result.cs
+++ MyOwnResultX/MyOwnResultX/Result.cs
@@ -1,7 +1,88 @@
-﻿namespace MyOwnResultX
-{
-    public class Class1
-    {
+﻿using System.Net.Http.Headers;
 
+namespace MyOwnResultX;
+
+public abstract class Result
+{
+    public bool IsSuccess {  get; protected set; }
+    public bool IsError => IsSuccess != true;
+
+    public List<Error> Errors { get; protected set; } = new();
+
+    protected Result() { }
+
+    public static Result<TValue> Ok<TValue>() => new();
+    public static Result<TValue> Ok<TValue>(TValue value) => new(value);
+    public static Result<TValue> Bad<TValue>(string message) => new(new Error(message));
+    public static Result<TValue> Bad<TValue>(string message, string code) => new(new Error(message, code));
+    public static Result<TValue> Bad<TValue>(Error error) => new(error);
+    public static Result<TValue> Bad<TValue>(List<Error> errors) => new(errors);
+
+}
+
+public class Result<TValue> : Result
+{
+    public TValue? Value { get; private set; }
+    public bool HasValue {  get; private set; }
+
+    internal Result() 
+    {
+        IsSuccess = true;
+        Value = default;
+        HasValue = false;
     }
+
+    internal Result(TValue? value)
+    {
+        IsSuccess = true;
+        Value = value;
+        HasValue = false;
+    }
+
+    internal Result(Error error)
+    {
+        IsSuccess = false;
+        Value = default;
+        HasValue = false;
+        Errors.Add(error);
+    }
+
+    internal Result (List<Error> errors)
+    {
+        IsSuccess = false;
+        Value = default;
+        HasValue = false;
+        Errors = errors;
+    }
+
+    public static implicit operator Result<TValue>(TValue value) => new(value);
+    public static implicit operator Result<TValue>(Error error) => new(error);
+    public static implicit operator Result<TValue>(List<Error> errors) => new(errors);
+
+    public void Deconstruct(out bool isSuccess, out TValue? value, out List<Error> errors)
+    {
+        isSuccess = IsSuccess;
+        value = Value;
+        errors = Errors;
+    }
+
+}
+
+public class Error
+{
+    public string Code { get; private set; }
+    public string Message { get; private set; }
+
+    public Error(string message, string code)
+    {
+        Message = message;
+        Code = code;
+    }
+
+    public Error(string message)
+    {
+        Message = message;
+        Code = string.Empty;
+    }
+        
 }
